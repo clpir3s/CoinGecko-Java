b38c56303ae1bd0f4913aa9bc3eeb71dfab9713f
clpir3s <chone.lopes@gmail.com>
2022-04-09 01:41:18 +0100

    implement Serializable

--- src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinData/Roi.java
+++ src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinData/Roi.java
@@ -1,12 +1,15 @@
 package com.litesoftwares.coingecko.domain.Coins.CoinData;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Roi {
+public class Roi implements Serializable {
+    private static final long serialVersionUID = 1669293150219020249L;
     @JsonProperty("times")
     private float times;
     @JsonProperty("currency")
--- src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinMarkets.java
+++ src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinMarkets.java
@@ -1,5 +1,7 @@
 package com.litesoftwares.coingecko.domain.Coins;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.litesoftwares.coingecko.domain.Coins.CoinData.Roi;
@@ -8,7 +10,9 @@ import lombok.*;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CoinMarkets {
+public class CoinMarkets implements Serializable {
+    private static final long serialVersionUID = 2669293150219020249L;
+    
     @JsonProperty("id")
     private String id;
     @JsonProperty("symbol")
