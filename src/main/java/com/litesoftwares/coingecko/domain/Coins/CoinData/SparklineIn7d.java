86f62cd1ae50aa7fb899ca90448ed6507545e7fd
clpir3s <chone.lopes@gmail.com>
2022-04-17 05:10:53 +0100

    add Serializable

--- src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinData/SparklineIn7d.java
+++ src/main/java/com/litesoftwares/coingecko/domain/Coins/CoinData/SparklineIn7d.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class SparklineIn7d {
+public class SparklineIn7d implements Serializable {
+    private static final long serialVersionUID = -2169293150219020249L;
     @JsonProperty("price")
     private List<Double> price;
 
